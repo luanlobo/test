44b5f7e6b44dbc883fa54ceae2e19eafb58898c0
malan <malan@192.168.1.18>
2012-01-28 04:58:22 -0500

    test2

--- index.php
+++ index.php
@@ -2,11 +2,18 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <script src="jquery-1.7.1.js"></script>
+        <script src="foo.js"></script>
+        <script>
+            $("#body").addClass(class)
+            
+        </script>
         <title></title>
     </head>
-    <body>
-        <?php
-        // put your code here
-        ?>
+    <body id="body"">
+          <div>
+              <script>$(document).ready(fn)</script>
+        </div>asas
     </body>
 </html>
+e
